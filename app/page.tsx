c324c49a8628774352b84ccb8cd7f2da7b67eb1f
chndrwali <chndrwali@gmail.com>
2024-04-24 16:55:23 +0700

    add: bg-home

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
-import Image from 'next/image';
-import { Redressed } from 'next/font/google';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import Undangan from '@/components/undangan';
-import { Suspense } from 'react';
-
-const redressed = Redressed({
-  subsets: ['latin'],
-  weight: '400',
-});
+import HomeComponent from '@/components/home';
 
 export default function Home() {
   return (
-    <section className="min-h-screen container mx-auto">
-      <div className="flex flex-col items-center">
-        <div className="my-4">
-          <Image src="" alt="Photo wedding" className="" />
-        </div>
-        <h1 className={`${redressed.className} text-5xl text-center font-bold `}>Cacan & Cinta</h1>
-        <h3 className="font-semibold">Yth. Bapak/Ibu/Saudara/I</h3>
-        <Suspense fallback={<span>Loading...</span>}>
-          <Undangan />
-        </Suspense>
-        <p className="capitalize text-muted-foreground">Tanpa mengurangi rasa hormat, kami mengundang anda untuk berhadir di acara pernikahan kami</p>
-        <Button asChild>
-          <Link href="/wedding">Buka Undangan</Link>
-        </Button>
-      </div>
-    </section>
+    <main>
+      <HomeComponent />
+    </main>
   );
 }
---
+++ components/home.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { Redressed } from 'next/font/google';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Undangan from '@/components/undangan';
+import { Suspense } from 'react';
+
+const redressed = Redressed({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+const HomeComponent = () => {
+  return (
+    <section className="min-h-screen bg-hero bg-no-repeat bg-cover container mx-auto">
+      <div className="flex flex-col items-center">
+        <div className="my-4">
+          <Image src="" alt="Photo wedding" className="" />
+        </div>
+        <h1 className={`${redressed.className} text-5xl text-center font-bold `}>Cacan & Cinta</h1>
+        <h3 className="font-semibold">Yth. Bapak/Ibu/Saudara/I</h3>
+        <Suspense fallback={<span>Loading...</span>}>
+          <Undangan />
+        </Suspense>
+        <p className="capitalize text-muted-foreground">Tanpa mengurangi rasa hormat, kami mengundang anda untuk berhadir di acara pernikahan kami</p>
+        <Button asChild>
+          <Link href="/wedding">Buka Undangan</Link>
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default HomeComponent;
