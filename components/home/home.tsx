7fee940b17c1bcb0ef00004a7516e96665f419aa
chndrwali <chndrwali@gmail.com>
2024-04-26 21:56:32 +0700

    add: wrap

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
 import { Redressed } from 'next/font/google';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Undangan from '@/components/home/undangan';
 import { Suspense } from 'react';
 import 'animate.css';
+import PageWrap from '../ui/pageWrap';
 
 const redressed = Redressed({
   subsets: ['latin'],
@@ -13,88 +13,39 @@ const redressed = Redressed({
 
 const HomeComponent = () => {
   return (
-    <>
-      <div className="fixed h-screen w-full overflow-hidden flex-col flex">
-        <div className="fixed h-screen w-full top-0 right-0 bottom-0 left-0 transform origin-center">
-          <div className="flex flex-col items-center justify-center mx-auto h-full border-8">
-            <div className="w-full max-w-[400px] h-screen">
-              <div>
-                <div>
-                  <ul>
-                    <li>
-                      <div className="relative">
-                        <div className="bg-cover min-h-screen rounded-lg" style={{ backgroundImage: 'url(/home/bg.webp)' }}>
-                          <div className="absolute inset-0 flex items-center justify-center">
-                            <Image src="/home/frame-tl.webp" className="absolute top-0 left-0 w-48 h-48 rounded-tl-lg animate__animated animate__fadeInDown  animate__slower" alt="frame" width={100} height={100} />
-                            <Image src="/home/frame-tr.webp" className="absolute top-0 right-0 w-48 h-48 rounded-tr-lg animate__animated animate__zoomIn animate__slower" alt="frame" width={100} height={100} />
-                            <Image src="/home/frame-bl.webp" className="absolute bottom-0 left-0 w-48 h-48 rounded-bl-lg animate__animated animate__zoomIn animate__slower" alt="frame" width={100} height={100} />
-                            <Image src="/home/frame-br.webp" className="absolute bottom-0 right-0 w-48 h-48 rounded-br-lg animate__animated animate__zoomIn animate__slower" alt="frame" width={100} height={100} />
-                          </div>
-                          <div className="flex justify-center items-center h-screen w-full">
-                            <div style={{ width: '100%' }}>
-                              <div className="text-center editable animate__animated animate__fadeInDown animate__slower mb-1 text-sm" style={{ letterSpacing: '0.2rem' }}>
-                                THE WEDDING OF
-                              </div>
-                              <div
-                                className="mb-4 flex flex-col justify-center animate__animated animate__zoomIn animate__slower image-editable"
-                                style={{ width: '80%', margin: 'auto', overflow: 'hidden', aspectRatio: '1/1', backgroundImage: 'url(/home/circle.webp)', backgroundSize: '100%' }}
-                              >
-                                <div className="text-center">
-                                  <div>
-                                    <h1 className={`${redressed.className} text-4xl text-center font-bold leading-none `}>
-                                      Ajeng <br /> & <br /> Aril
-                                    </h1>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="text-center mx-auto relative z-10" style={{ maxWidth: '300px' }}>
-                                <div className="text-center mb-3 p-2 animate__animated animate__zoomIn animate__slower rounded" style={{ fontSize: '14px' }}>
-                                  <div className="editable mb-1">
-                                    Dear
-                                    <br />
-                                    Mr/Ms/Brother/Sisters
-                                  </div>
-                                  <Suspense fallback={<span>Loading...</span>}>
-                                    <Undangan />
-                                  </Suspense>
-                                </div>
-                                <div className="z-10">
-                                  <Button asChild className="z-10">
-                                    <Link href="/wedding">Open Invitation</Link>
-                                  </Button>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              {/* Navigation */}
-              {/* end navigation */}
-            </div>
+    <PageWrap>
+      <div className="text-center editable animate__animated animate__fadeInDown animate__slower mb-1 text-sm" style={{ letterSpacing: '0.2rem' }}>
+        THE WEDDING OF
+      </div>
+      <div
+        className="mb-4 flex flex-col justify-center animate__animated animate__zoomIn animate__slower image-editable"
+        style={{ width: '80%', margin: 'auto', overflow: 'hidden', aspectRatio: '1/1', backgroundImage: 'url(/home/circle.webp)', backgroundSize: '100%' }}
+      >
+        <div className="text-center">
+          <div>
+            <h1 className={`${redressed.className} text-4xl text-center font-bold leading-none `}>
+              Ajeng <br /> & <br /> Aril
+            </h1>
           </div>
         </div>
       </div>
-
-      {/* <div className="flex flex-col items-center">
-        <div className="my-4 p-4 h-40 w-10 rounded-xl">
-          <Image src="" alt="Photo wedding" className="" width={40} height={160} />
+      <div className="text-center mx-auto relative z-10" style={{ maxWidth: '300px' }}>
+        <div className="text-center mb-3 p-2 animate__animated animate__zoomIn animate__slower rounded" style={{ fontSize: '14px' }}>
+          <div className="editable mb-1">
+            Dear
+            <br />
+            Mr/Ms/Brother/Sisters
+          </div>
+          <Suspense fallback={<span>Loading...</span>}>
+            <Undangan />
+          </Suspense>
         </div>
-        <h1 className={`${redressed.className} text-5xl text-center font-bold `}>Cacan & Cinta</h1>
-        <h3 className="font-semibold">Yth. Bapak/Ibu/Saudara/I</h3>
-        <Suspense fallback={<span>Loading...</span>}>
-          <Undangan />
-        </Suspense>
-        <p className="capitalize text-muted-foreground">Tanpa mengurangi rasa hormat, kami mengundang anda untuk berhadir di acara pernikahan kami</p>
-        <Button asChild>
-          <Link href="/wedding">Buka Undangan</Link>
+
+        <Button asChild className="z-10">
+          <Link href="/wedding">Open Invitation</Link>
         </Button>
-      </div> */}
-    </>
+      </div>
+    </PageWrap>
   );
 };
 
--- components/thanks/thanksPage.tsx
+++ components/thanks/thanksPage.tsx
@@ -1,46 +1,12 @@
-import Image from 'next/image';
-
 const ThanksPage = () => {
   return (
-    <section className="fixed h-screen w-full overflow-hidden flex-col flex">
-      <div className="fixed h-screen w-full top-0 right-0 bottom-0 left-0 transform origin-center">
-        <div className="flex flex-col items-center justify-center mx-auto h-full border-8">
-          <div className="w-full max-w-[400px] h-screen">
-            <div>
-              <div>
-                <ul>
-                  <li>
-                    <div className="relative">
-                      <div className="bg-cover min-h-screen rounded-lg" style={{ backgroundImage: 'url(/home/bg.webp)' }}>
-                        <div className="absolute inset-0 flex items-center justify-center">
-                          <Image src="/home/frame-tl.webp" className="absolute top-0 left-0 w-48 h-48 rounded-tl-lg animate__animated animate__fadeInDown  animate__slower" alt="frame" width={100} height={100} />
-                          <Image src="/home/frame-tr.webp" className="absolute top-0 right-0 w-48 h-48 rounded-tr-lg animate__animated animate__zoomIn animate__slower" alt="frame" width={100} height={100} />
-                          <Image src="/home/frame-bl.webp" className="absolute bottom-0 left-0 w-48 h-48 rounded-bl-lg animate__animated animate__zoomIn animate__slower" alt="frame" width={100} height={100} />
-                          <Image src="/home/frame-br.webp" className="absolute bottom-0 right-0 w-48 h-48 rounded-br-lg animate__animated animate__zoomIn animate__slower" alt="frame" width={100} height={100} />
-                        </div>
-                        <div className="flex justify-center items-center h-screen w-full">
-                          <div style={{ width: '100%' }}>
-                            {/* content */}
-                            <div className="grid h-auto place-content-center bg-background px-4 relative">
-                              <div className="text-center">
-                                <h1 className="text-4xl font-black text-gray-200">it&apos;s our hope and pleasure to have you on our big day</h1>
-                                <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Kind Regards</p>
-                                <p className="mt-4 text-gray-500">Ajeng & Aril</p>
-                              </div>
-                            </div>
-                            {/* content */}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="grid h-auto place-content-center px-4 relative">
+      <div className="text-center">
+        <h1 className="text-3xl font-black text-gray-200">it&apos;s our hope and pleasure to have you on our big day</h1>
+        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Kind Regards</p>
+        <p className="mt-4 text-gray-500">Ajeng & Aril</p>
       </div>
-    </section>
+    </div>
   );
 };
 
