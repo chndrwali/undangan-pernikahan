e3d41a5039635c9bab24c7dd73ca7231201da2ee
chndrwali <chndrwali@gmail.com>
2024-05-07 16:33:05 +0700

    fix msic

--- components/home/music-button.tsx
+++ components/home/music-button.tsx
@@ -10,7 +10,7 @@ interface Props {
 const MusicButton: React.FC<Props> = ({ audioRef, isPlaying, togglePlayPause }) => {
   return (
     <div
-      className={`fixed min-h-[44px] min-w-[44px] z-50 cursor-pointer justify-center items-center bottom-6 p-4 right-10 h-11 w-11 bg-white bg-footer rounded-full shadow shadow-dark/50 hover:shadow-lg hover:shadow-dark/30 hover:opacity-90 transition-all duration-200 ease-in-out `}
+      className={`fixed min-h-[44px] min-w-[44px] z-50 cursor-pointer justify-center items-center bottom-6 p-4 right-6 h-11 w-11 bg-white bg-footer rounded-full shadow shadow-dark/50 hover:shadow-lg hover:shadow-dark/30 hover:opacity-90 transition-all duration-200 ease-in-out `}
     >
       <button onClick={togglePlayPause} className="flex items-center justify-center h-full w-full">
         {isPlaying ? <Music size={20} className="animate-spin" /> : <Pause size={20} className="line-through" />}
